2b68575b59cfcc699362b71d2606e369cad4ed49
Hunter Rancourt <hunter.rancourt@gmail.com>
2024-01-11 10:13:31 -0600

    feat: add styleint config

---
+++ .stylelintrc.cjs
@@ -0,0 +1,8 @@
+// TODO: Update this to use CIG's linting
+module.exports = {
+  extends: ['@roots/bud-sass/config/stylelint'],
+  rules: {
+    'import-notation': null,
+    'no-empty-source': null,
+  },
+};
